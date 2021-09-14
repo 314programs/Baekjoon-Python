1b43614cbd5cf6d407603b883c313640badb89eb
314programs <88335174+314programs@users.noreply.github.com>
2021-09-14 19:47:13 +0800

    Update Q1107.py

--- Q1107.py
+++ Q1107.py
@@ -1,3 +1,4 @@
+#Set up lists of functional buttons
 Channel = int(input())
 BrokenNum = int(input())
 if BrokenNum != 0:
@@ -8,15 +9,17 @@ else:
 
 CurrentChannel = 100
 
+#Because speed is not needed, loop it until the maximum point to get the answer.
 result = [1000000, 0]
 for a in range(1000000):
   Count = 0
+#Checking if the number can be made with functional buttons
   for char in str(a):
     if int(char) in FunctionalButton:
       Count += 1
-  
+#Checking if the current result is better(Smaller) than the previous
   if Count == len(str(a)):
     if abs(Channel-a) < result[0]:
       result = [abs(Channel-a), a]
-
+#Print minimum between pressing arrow buttons and number buttons to pressing only arrow buttons
 print(min(result[0]+len(str(result[1])), abs(CurrentChannel-Channel)))
