b7e7bbcbb2ab6cdc8a734679aadaead5eb218077
314programs <88335174+314programs@users.noreply.github.com>
2021-09-14 19:38:28 +0800

    Update Q11726.py

--- Q11726.py
+++ Q11726.py
@@ -1,6 +1,8 @@
+#Take input and set up a list
 Width = int(input())
 DP = [int(i) for i in range(Width+1)]
 
+#Use memoization to stored added values
 for i in range(3, Width+1):
   DP[i] = DP[i-1] + DP[i-2]
 print(DP[Width]%10007)
