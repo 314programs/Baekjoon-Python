a217fa4b9c011242064e1f4a98e0a85dc15435d8
314programs <88335174+314programs@users.noreply.github.com>
2021-09-14 19:41:27 +0800

    Update Q9461.py

--- Q9461.py
+++ Q9461.py
@@ -1,9 +1,10 @@
+#Input and setup
 TestCase = int(input())
 for a in range(TestCase):
   Answer = int(input())
   Dp = [1,1,1]
-
+#Using memoization to store answers
   for i in range(3,Answer):
     Dp.append(Dp[i-3]+Dp[i-2])
-
+#Print the last(index of Answer-1) element
   print(Dp[-1])
