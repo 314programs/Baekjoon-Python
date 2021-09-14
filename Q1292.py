f3284e9117716a5cf400c4dcd46c1aae5bb12db8
314programs <88335174+314programs@users.noreply.github.com>
2021-09-14 19:40:15 +0800

    Update Q1292.py

--- Q1292.py
+++ Q1292.py
@@ -1,10 +1,17 @@
+#Input and set up
 Start, End = map(int, input().split())
 NumList = []
+#For stopping the loop
+Alarm = False
+#Loop to make a list
 for a in range(1,End+1):
   for b in range(a):
     NumList.append(a)
-
-  if len(NumList) == End:
+    if len(NumList) >= End:
+      Alarm = True
+      break
+  if Alarm:
     break
 
+#Since speed is not needed, sum is used
 print(sum(NumList[Start-1:End]))
