6206f7df7727a0c9a66320bb222a3a71e029bd9b
314programs <88335174+314programs@users.noreply.github.com>
2021-09-14 19:35:52 +0800

    Update Q11659.py

--- Q11659.py
+++ Q11659.py
@@ -1,13 +1,13 @@
 import sys
-
+#Take input
 NumCount, SumCount = map(int, sys.stdin.readline().split())
 NumList = list(map(int, sys.stdin.readline().split()))
 
 Sums = [0]
-
+#Make a list of added values
 for i in range(NumCount):
   Sums.append(Sums[-1] + NumList[i])
-
+#Use the list of added values to calculate the sum
 for i in range(SumCount):
   Start, End = map(int, sys.stdin.readline().split())
   sys.stdout.write(str(Sums[End] - Sums[Start-1]) + '\n')
