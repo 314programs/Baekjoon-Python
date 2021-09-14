b6f4e4038e2aafc1e0cb881016d80ca6c19fc87c
314programs <88335174+314programs@users.noreply.github.com>
2021-09-14 19:37:00 +0800

    Update Q1004.py

--- Q1004.py
+++ Q1004.py
@@ -1,4 +1,5 @@
 TestCase = int(input())
+#Input and set up
 for x in range(TestCase):
   StartX, StartY, EndX, EndY = map(int, input().split())
   PlanetNumber = int(input())
@@ -6,7 +7,8 @@ for x in range(TestCase):
   PlanetList = []
   for i in range(PlanetNumber):
     PlanetList.append(list(map(int, input().split()))) 
-
+#Use the graph of a circle to find out if starting point and ending point is inside the radius
+#Only add count when one is inside
   for item in PlanetList:
     if (StartX - item[0])**2 + (StartY - item[1])**2 < item[2]**2 and (EndX - item[0])**2 + (EndY - item[1])**2 < item[2]**2:
       Result += 0
