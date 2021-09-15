579aa97927eadb22b4fdb59f7397ff455f861bbd
314programs <88335174+314programs@users.noreply.github.com>
2021-09-15 14:16:50 +0800

    Create Q5430.py

---
+++ Q5430.py
@@ -0,0 +1,40 @@
+import collections
+import sys
+
+CaseCount = int(sys.stdin.readline().rstrip())
+for x in range(CaseCount):
+  Commend = sys.stdin.readline().rstrip()
+  ListLen = int(sys.stdin.readline().rstrip())
+  ListString = sys.stdin.readline().rstrip()[1:-1]
+  if ListLen > 0:
+    TheList = collections.deque(list(map(int, ListString.split(sep = ','))))
+  else:
+    TheList = []
+  Results = []
+  RCount = 0
+  Error = False
+
+  for i in range(len(Commend)):
+    if len(TheList) == 0 and Commend[i] == 'D':
+      Error = True
+      break
+    if Commend[i] == 'R':
+      RCount += 1
+
+    if RCount == 0 or RCount%2==0:
+      if Commend[i] == 'D':
+        TheList.popleft()
+    else:
+      if Commend[i] == 'D':
+        TheList.pop()
+
+  if not Error:
+    if RCount == 0 or RCount%2==0:
+      for i in range(len(TheList)):
+        Results.append(TheList[i])
+    else:
+      for i in range(len(TheList)-1, -1, -1):
+        Results.append(TheList[i])
+    sys.stdout.write(str(Results).replace(' ', '') + '\n')
+  else:
+    sys.stdout.write('error' + '\n')
