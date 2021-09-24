b874e2ac9dfaf4699ad73dc8c2d3791159b5c2e7
314programs <88335174+314programs@users.noreply.github.com>
2021-09-24 09:25:34 +0800

    Update Q1254.py

--- Q1254.py
+++ Q1254.py
@@ -1,14 +1,15 @@
 Phase = str(input())
 PelinList = []
+#If the number is already a pelindrome, print its length and exit
 if Phase[::-1] == Phase:
   print(len(Phase))
-
+#If it is not pelindrome run a loop to make a list of pelindromes from the number
 else:
   for i in range(1, len(Phase)+1):
     Temp = Phase[:i]
     PelinList.append(Temp + Temp[::-1])
     PelinList.append(Temp + Temp[:i-1][::-1])
-
+#For items inside pelindrome list, check to see if item's front has the original phase inside
   ResultList = []
 
   for item in PelinList:
@@ -16,5 +17,5 @@ else:
       if item[:len(Phase)] == Phase:
         ResultList.append(len(item))
 
-
+#Print the minimum from the lengths
   print(min(ResultList))
