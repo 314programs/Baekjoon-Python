04bfa4e778d4b7b30f4482779dca623ddab6fc8a
314programs <88335174+314programs@users.noreply.github.com>
2022-01-03 16:34:02 +0800

    Create Q4562.py

---
+++ Q4562.py
@@ -0,0 +1,8 @@
+#School started(with exams), less time to code...
+testcase = int(input())
+for i in range(testcase):
+    x,y = map(int, input().split())
+    if x>=y:
+        print('MMM BRAINS')
+    else:
+        print('NO BRAINS')
