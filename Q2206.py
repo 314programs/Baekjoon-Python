1000ff7f7513a807f972e1b0009fc017f4cf5e9e
314programs <88335174+314programs@users.noreply.github.com>
2022-01-09 14:15:04 +0800

    Update Q2206.py

--- Q2206.py
+++ Q2206.py
@@ -1,55 +1,61 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 Height, Width = map(int, input().split())
 Graph = []
 Visited = []
 
-for y in range(Height):
-    Graph.append(list(map(int, input())))
+for i in range(Height):
+    Graph.append(list(map(int, input().strip())))
     Visited.append([0]*Width)
     
+#Basic direction
 Dy = [0,0,1,-1]
 Dx = [1,-1,0,0]
 
-#[0]: Wall or space, [1]: How much traveled, [2]: wall broken
-for y in range(Height):
-    for x in range(Width):
-        if Graph[y][x] == 0:
-            Visited[y][x] = [0, 0, 0]
-        elif Graph[y][x] == 1:
-            Visited[y][x] = [-1, 0, 0]
-            
-            
+for h in range(Height):
+    for w in range(Width):
+        if Graph[h][w] == 0:
+            #[0]: wall or empty space, [1]: distance travelled, [2]: wall broken
+            Visited[h][w] = [0,0,False]
+        else:
+            Visited[h][w] = [-1,0,False]
+
+Visited[0][0] = [0,0,False]
+
 queue = deque([[0,0]])
-Visited[0][0] = [-2, 0, 0]
-#BFS
-while queue:
+wallqueue = deque([])
+while queue or wallqueue:
     Temp = queue.popleft()
+    
     for i in range(4):
         Y = Dy[i] + Temp[0]
         X = Dx[i] + Temp[1]
         
-        #Append if it is in the graph and smaller or not visited than visited value
-        if -1<Y<Height and -1<X<Width and Visited[Temp[0]][Temp[1]][2] < 2 and (Visited[Y][X][1] == 0 or Visited[Temp[0]][Temp[1]][1] + 1 < Visited[Y][X][1]):
+        #If not visited or have breaken wall to find a shorter path
+        if -1<Y<Height and -1<X<Width and (Visited[Y][X][1] == 0 or (Visited[Temp[0]][Temp[1]][2] == True and Visited[Y][X][1] > Visited[Temp[0]][Temp[1]][1]+1)):
+            #If space
             if Visited[Y][X][0] == 0:
                 Visited[Y][X][1] = Visited[Temp[0]][Temp[1]][1] + 1
-                Visited[Y][X][2] = Visited[Temp[0]][Temp[1]][2]
-                
-                #Append to left when no wall is broken, so that if it doesn't work it can try with broken wall
-                if Visited[Temp[0]][Temp[1]][2] == 0:
-                    queue.appendleft([Y,X])
+                if Visited[Temp[0]][Temp[1]][2] == True:
+                    Visited[Y][X][2] = True
+                    wallqueue.append([Y,X])
                 else:
                     queue.append([Y,X])
-             
-            elif Visited[Y][X][0] == -1:
+            #If wall
+            elif Visited[Y][X][0] == -1 and Visited[Temp[0]][Temp[1]][2] == False:
                 Visited[Y][X][1] = Visited[Temp[0]][Temp[1]][1] + 1
-                Visited[Y][X][2] = Visited[Temp[0]][Temp[1]][2] + 1
-                queue.append([Y,X])
+                Visited[Y][X][2] = True
+                wallqueue.append([Y,X])
 
-#Print
-Count = Visited[Height-1][Width-1][1]
-if Count == 0 and Height != 1 and Width != 1:
+    if len(queue) == 0:
+        queue = wallqueue
+        wallqueue = deque([])
+    
+result = Visited[Height-1][Width-1][1]
+
+if result == 0:
     print(-1)
 else:
-    print(Count + 1)
-    
+    print(result+1)
