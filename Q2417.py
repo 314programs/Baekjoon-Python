75e9fca1bc78d461d92d429c39ba484add1a78be
314programs <88335174+314programs@users.noreply.github.com>
2022-02-23 09:42:33 +0800

    Update Q2417.py

--- Q2417.py
+++ Q2417.py
@@ -1,3 +1,15 @@
-import math
+#Turns out it needs binary search
 num = int(input())
-print(math.ceil(num**0.5))
+
+start = 0
+end = num
+
+while end >= start:
+    mid = (start + end) // 2
+    
+    if mid ** 2 < num:
+        start = mid + 1
+    else:
+        end = mid - 1
+        
+print(end+1)
