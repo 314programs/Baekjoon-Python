a584bb8e882bf53c20c57542436097ae43b52006
314programs <88335174+314programs@users.noreply.github.com>
2022-03-14 20:00:42 +0800

    Create Q1463.py

---
+++ Q1463.py
@@ -0,0 +1,12 @@
+#Need to practice DP
+#This is bottom up
+num = int(input())
+dp = [0 for v in range(num+1)]
+
+for i in range(2, num+1):
+    dp[i] = dp[i-1] + 1
+    if i%3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i%2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+print(dp[num])
