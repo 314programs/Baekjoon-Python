2d91ca7c5a8e67a65f68ef95d3ac8f9ede6fbc17
314programs <88335174+314programs@users.noreply.github.com>
2022-03-17 11:54:29 +0800

    Update Q15988.py

--- Q15988.py
+++ Q15988.py
@@ -5,14 +5,16 @@ testcase = int(input())
 Divider = 1000000009
 
 dp = [0 for v in range(1000001)]
+#Set initial value
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
+#Set up array to save time
 for i in range(4, 1000001):
     dp[i] = (dp[i-1] + dp[i-2] + dp[i-3])%Divider
-        
 
+#Print for each
 for case in range(testcase):
     num = int(input())
     print(dp[num]%Divider)
